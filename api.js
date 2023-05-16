f9f3b732db0b2d10aa52b7dd7ce0e10f66460071
Tom Sproull <64221818+sprouller@users.noreply.github.com>
2023-05-16 16:57:10 +0100

    Create api.js

---
+++ api.js
@@ -0,0 +1,67 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "183E3D348FF1624BBF4F6711422CF54B");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'manual'
+};
+
+var container = document.getElementById("table");
+
+fetch("https://live.bloodstockreports.co.uk/4DCGI/EANDRAPI/RacesForList?entryDays=6&resultsMax=40&resultsOffset=0", requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    var headers = ["Horse", "Race Course", "Race Details", "Race Date", "Race Time"];
+    var columnWidths = ["is-width-medium", "is-width-large", "is-width-xlarge", "is-width-medium", "is-width-medium"];
+
+    var table = document.createElement("div");
+    table.classList.add("table2_list");
+
+    var headerRow = document.createElement("div");
+    headerRow.classList.add("table2_header-row");
+
+    for (var i = 0; i < headers.length; i++) {
+      var headerCell = document.createElement("div");
+      headerCell.classList.add("table2_column", "is-header-column", columnWidths[i]);
+      headerCell.textContent = headers[i];
+      headerRow.appendChild(headerCell);
+    }
+
+    table.appendChild(headerRow);
+
+    data.entries.forEach(entry => {
+      var rowDiv = document.createElement("div");
+      rowDiv.classList.add("table2_item");
+
+      var horseCell = document.createElement("div");
+      horseCell.classList.add("table2_column", "is-width-medium", "text-weight-semibold");
+      horseCell.textContent = entry.animalName;
+      rowDiv.appendChild(horseCell);
+
+      var raceCourseCell = document.createElement("div");
+      raceCourseCell.classList.add("table2_column", "is-width-large", "text-weight-light");
+      raceCourseCell.textContent = entry.raceCourse;
+      rowDiv.appendChild(raceCourseCell);
+
+      var raceDetailsCell = document.createElement("div");
+      raceDetailsCell.classList.add("table2_column", "is-width-xlarge", "text-weight-semibold");
+      raceDetailsCell.textContent = entry.raceDetails;
+      rowDiv.appendChild(raceDetailsCell);
+
+      var raceDateCell = document.createElement("div");
+      raceDateCell.classList.add("table2_column", "is-width-medium", "text-weight-light");
+      raceDateCell.textContent = entry.raceDate;
+      rowDiv.appendChild(raceDateCell);
+
+      var raceTimeCell = document.createElement("div");
+      raceTimeCell.classList.add("table2_column", "is-width-medium", "text-weight-light");
+      raceTimeCell.textContent = entry.raceTime;
+      rowDiv.appendChild(raceTimeCell);
+
+      table.appendChild(rowDiv);
+    });
+
+    container.appendChild(table);
+  })
+  .catch(error => console.log('error', error));
